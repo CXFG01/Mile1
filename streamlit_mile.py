5402d8a801489cc0687e69631e83592322745b94
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-01 19:04:58 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -1,1 +1,16 @@
 import streamlit as st
+import pandas as pd
+import joblib
+
+st.header("Heart Disease App")
+
+st.markdown(
+
+Hey there!
+
+This app here uses the power of machine learning(traditional and neural) to help predict the affliction of an individual with Coronary Heart Disease or Myocardial Infraction(Heart Attack).
+
+# By no means is this a medical tool and only a certified doctor can inquire further in your problem.
+#Irrelevant of the result, have periodic visits at the doctor to know more about the body's condition.
+
+)
