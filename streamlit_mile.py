a42cfd3f682b19669d17e6e5cc0f6ed3ad8638b6
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-01 19:06:16 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -4,7 +4,7 @@ import joblib
 
 st.header("Heart Disease App")
 
-st.markdown(
+st.markdown("""
 
 Hey there!
 
@@ -13,4 +13,4 @@ This app here uses the power of machine learning(traditional and neural) to help
 # By no means is this a medical tool and only a certified doctor can inquire further in your problem.
 #Irrelevant of the result, have periodic visits at the doctor to know more about the body's condition.
 
-)
+""")
