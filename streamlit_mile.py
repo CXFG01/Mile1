e456cd2c0e10fe2942c7fd03a3fef0007fde874b
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-01 19:54:50 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -10,8 +10,9 @@ st.markdown("""
 <p style="font-size: 22px;">Hey there! </p>
 
 <p style="font-size: 22px;">This app here uses the power of machine learning(traditional and neural) to help predict the affliction of an individual with Coronary Heart Disease or Myocardial Infraction(Heart Attack). </p>
-
+<p style="font-size: 26px;">
 # By no means is this a medical tool and only a certified doctor can inquire further in your problem.
 # Irrelevant of the result, have periodic visits at the doctor to know more about the body's condition.
+</p>
 
 """, unsafe_allow_html=True)
