dad43303ac90bb82489e36c03de1804aead06ca8
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-01 21:08:46 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -8,31 +8,31 @@ st.header("Heart Disease App")
 def start():
 	st.markdown(""" <p style="font-size: 22px;">Please answer the questions below: </p>""", unsafe_allow_html=True)
 	st.markdown(""" <p style="font-size: 22px;">What is your BMI? </p>""", unsafe_allow_html=True)
-	BMI= st.number_input("Insert BMI",min_value=10.000, max_value=100.000)
+	BMI= st.number_input("Insert BMI",min_value=10.000, max_value=100.000, key=1)
 	
 	st.markdown(""" <p style="font-size: 22px;">Have you smoked at least 100 cigarettes in your entire life? [Note: 5 packs = 100 cigarettes]</p>""", unsafe_allow_html=True)
-	Smoking= st.selectbox("Select your answer", ("No", "Yes"))
+	Smoking= st.selectbox("Select your answer", ("No", "Yes"), key=2)
 	
 	st.markdown(""" <p style="font-size: 22px;">Heavy drinkers (adult men having more than 14 drinks per week and adult women having more than 7 drinks per week</p>""", unsafe_allow_html=True)
-	AlcoholDrinking= st.selectbox("Select your answer", ("No", "Yes"))
+	AlcoholDrinking= st.selectbox("Select your answer", ("No", "Yes"), key=3)
 	
 	st.markdown(""" <p style="font-size: 22px;">Told/had a stroke?</p>""", unsafe_allow_html=True)
-	Stroke= st.selectbox("Select your answer", ("No", "Yes"))
+	Stroke= st.selectbox("Select your answer", ("No", "Yes"), key=4)
 	
 	st.markdown(""" <p style="font-size: 22px;">Now thinking about your physical health, which includes physical illness and injury, for how many days during the past 30 days was your physical health not good? (0-30 days)</p>""", unsafe_allow_html=True)
-	PhysicalHealth= st.number_input("Insert days",min_value=0.00, max_value=30.00)
+	PhysicalHealth= st.number_input("Insert days",min_value=0.00, max_value=30.00, key=5)
 	
 	st.markdown(""" <p style="font-size: 22px;">Thinking about your mental health, for how many days during the past 30 days was your mental health not good? (0-30 days)</p>""", unsafe_allow_html=True)
-	MentalHealth= st.number_input("Insert days",min_value=0.00, max_value=30.00)
+	MentalHealth= st.number_input("Insert days",min_value=0.00, max_value=30.00, key=6)
 	
 	st.markdown(""" <p style="font-size: 22px;">Do you have serious difficulty walking or climbing stairs?</p>""", unsafe_allow_html=True)
-	DiffWalking= st.selectbox("Select your answer", ("No", "Yes"))
+	DiffWalking= st.selectbox("Select your answer", ("No", "Yes"), key=7)
 	
 	st.markdown(""" <p style="font-size: 22px;">Are you male or female?</p>""", unsafe_allow_html=True)
-	Sex= st.selectbox("Select your answer", ("No", "Yes"))
+	Sex= st.selectbox("Select your answer", ("No", "Yes"), key=8)
 	
 	st.markdown(""" <p style="font-size: 22px;">In what age category do you fit in?</p>""", unsafe_allow_html=True)
-	AgeCategory = st.selectbox("Select your answer", ("No", "Yes"))
+	AgeCategory = st.selectbox("Select your answer", ("No", "Yes"), key=9)
 
 
 if st.button("Proceed"):
