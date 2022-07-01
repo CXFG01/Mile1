39e726eea75beb518604aecb8444c7f377bbcf80
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-01 19:42:25 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -14,4 +14,4 @@ Hey there!
 # By no means is this a medical tool and only a certified doctor can inquire further in your problem.
 # Irrelevant of the result, have periodic visits at the doctor to know more about the body's condition.
 
-""")
+""", unsafe_allow_html=True)
