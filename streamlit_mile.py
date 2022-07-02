85dd59b644e7098432356863b06bf120b71964a9
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-02 10:17:02 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -48,7 +48,8 @@ def start():
 		results=BMI, Smoking, AlcoholDrinking, Stroke, PhysicalHealth, MentalHealth, DiffWalking, Sex, AgeCategory
 		if st.form_submit_button(label='Submit', on_click=state()):
 			return results
-if not st.button("Proceed", key='pro') and st.session_state.state!=0:
+butt1=st.button("Proceed", key='pro')
+if not butt1 and st.session_state.state!=0:
 	st.markdown("""
 	<p style="font-size: 22px;">Hey there! </p>
 	
@@ -61,7 +62,7 @@ if not st.button("Proceed", key='pro') and st.session_state.state!=0:
 	<p style="font-size: 22px;">Now, let's see what the AI says about your situation! (Press Proceed at the top) </p>
 	""", unsafe_allow_html=True)
 	
-elif st.button("Proceed", key='pro2') and st.session_state.state!=0 :
+elif butt1 and st.session_state.state!=0 :
 	m=str(start())
 	st.write(m)
   
