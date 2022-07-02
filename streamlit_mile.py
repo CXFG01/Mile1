ae45a3cac50fd191c514a1a772868caf04ac4448
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-02 12:00:47 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -17,9 +17,9 @@ if 'state' not in st.session_state:
 if 'form' not in st.session_state :
     st.session_state.form = 0
 
-def add():
-	st.session_state.k += 1
-
+def add(value):
+	lis.append(value)
+	
 def state():
 	st.session_state.state = 1
 	st.session_state.form = 1
@@ -30,72 +30,71 @@ def start():
 	with st.form(key='form1'):
 		st.markdown(""" <p style="font-size: 22px;">Please answer the questions below: </p>""", unsafe_allow_html=True)
 		st.markdown(""" <p style="font-size: 22px;">What is your BMI? </p>""", unsafe_allow_html=True)
-		BMI= st.number_input("Insert BMI",min_value=10.000, max_value=100.000, key=1, on_change=add())
-		lis.append(BMI)
+		BMI= st.number_input("Insert BMI",min_value=10.000, max_value=100.000, key=1, on_change=add(BMI))
 	
 		st.markdown(""" <p style="font-size: 22px;">Have you smoked at least 100 cigarettes in your entire life? [Note: 5 packs = 100 cigarettes]</p>""", unsafe_allow_html=True)
 		Smoking= st.selectbox("Select your answer", ("No", "Yes"), key=2)
-		lis.append(Smoking)
-	
+
+		
 		st.markdown(""" <p style="font-size: 22px;">Are you a heavy drinker? (Adult men having more than 14 drinks per week and adult women having more than 7 drinks per week</p>""", unsafe_allow_html=True)
 		AlcoholDrinking= st.selectbox("Select your answer", ("No", "Yes"), key=3)
-		lis.append(AlcoholDrinking)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Ever told/you had stroke?</p>""", unsafe_allow_html=True)
 		Stroke= st.selectbox("Select your answer", ("No", "Yes"), key=4)
-		lis.append(Stroke)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Now thinking about your physical health, which includes physical illness and injury, for how many days during the past 30 days was your physical health not good? (0-30 days)</p>""", unsafe_allow_html=True)
 		PhysicalHealth= st.number_input("Insert days",min_value=0.00, max_value=30.00, key=5)
-		lis.append(PhysicalHealth)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Thinking about your mental health, for how many days during the past 30 days was your mental health not good? (0-30 days)</p>""", unsafe_allow_html=True)
 		MentalHealth= st.number_input("Insert days",min_value=0.00, max_value=30.00, key=6)
-		lis.append(MentalHealth)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Do you have serious difficulty walking or climbing stairs?</p>""", unsafe_allow_html=True)
 		DiffWalking= st.selectbox("Select your answer", ("No", "Yes"), key=7)
-		lis.append(DiffWalking)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Are you male or female?</p>""", unsafe_allow_html=True)
 		Sex= st.selectbox("Select your answer", ("Female", "Male"), key=8)
-		lis.append(Sex)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">In what age category do you fit in?</p>""", unsafe_allow_html=True)
 		AgeCategory = st.selectbox("Select your answer", ("18-24", "25-29","30-34","35-39","40-44","45-59","50-54","55-59","60-64","65-69","70-74","75-79","80 or older"), key=9)
-		lis.append(AgeCategory)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Race</p>""", unsafe_allow_html=True)
 		Race = st.selectbox("Select your answer", ('American Indian/Alaskan Native', 'Asian', 'Black', 'Hispanic', 'Other', 'White'), key=10)
-		lis.append(Race)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Ever Told/Had diabetes?p>""", unsafe_allow_html=True)
 		Diabetic= st.selectbox("Select your answer", ('No', 'No, borderline diabetes', 'Yes', 'Yes (during pregnancy)'), key=11)
-		lis.append(Diabetic)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Have you been engaging in physical activity or exercise during the past 30 days other than your regular job</p>""", unsafe_allow_html=True)
 		PhysicalActivity= st.selectbox("Select your answer", ('No','Yes'), key=12)
-		lis.append(PhysicalActivity)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Would you say that in general your health is...</p>""", unsafe_allow_html=True)
 		GenHealth= st.selectbox("Select your answer", ('Excellent','Very good' ,'Good', 'Fair', 'Poor'), key=13)
-		lis.append(GenHealth)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">On average, how many hours of sleep do you get in a 24-hour period?</p>""", unsafe_allow_html=True)
 		SleepTime = st.number_input("Time",min_value=1.00, max_value=24.00, key=14)
-		lis.append(SleepTime)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Ever told/you had asthma?</p>""", unsafe_allow_html=True)
 		Asthma= st.selectbox("Select your answer", ("No", "Yes"), key=15)
-		lis.append(Asthma)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Not including kidney stones, bladder infection or incontinence, were you ever told you had kidney disease?</p>""", unsafe_allow_html=True)
 		KidneyDisease= st.selectbox("Select your answer", ("No", "Yes"), key=16)
-		lis.append(KidneyDisease)
+
 		
 		st.markdown(""" <p style="font-size: 22px;">Ever told/you had skin cancer?</p>""", unsafe_allow_html=True)
 		SkinCancer = st.selectbox("Select your answer", ("No", "Yes"), key=17)
-		lis.append(SkinCancer)
+
 		
 				
 		st.form_submit_button(label='Submit', on_click=state())
