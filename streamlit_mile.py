dc3aa304a1ce8593ab9e7e3f27508a21a4a7ab41
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-02 08:18:49 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -33,10 +33,12 @@ def start():
 	
 	st.markdown(""" <p style="font-size: 22px;">In what age category do you fit in?</p>""", unsafe_allow_html=True)
 	AgeCategory = st.selectbox("Select your answer", ("18-24", "25-29","30-34","35-39","40-44","45-59","50-54","55-59","60-64","65-69","70-74","75-79","80 or older"), key=9)
-
+	
+	return BMI, Smoking, AlcoholDrinking, Stroke, PhysicalHealth, MenalHealth, DiffWalking, Sex, AgeCategory
 
 if st.button("Proceed"):
-	start()
+	m=start()
+	print(m)
 else:
 	st.markdown("""
 	<p style="font-size: 22px;">Hey there! </p>
