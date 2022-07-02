6e09c7c081dd8d4d84a4839d263161f712bd19e0
CXFG01 <108541930+CXFG01@users.noreply.github.com>
2022-07-02 13:20:15 +0100

    Update streamlit_mile.py

--- streamlit_mile.py
+++ streamlit_mile.py
@@ -33,18 +33,17 @@ def predict(results):
 #number = st.number_input('Insert a number')
 #st.write('The current number is ', number)
 
-if st.session_state.state==0:
-	st.markdown("""
-	<p style="font-size: 22px;">Hey there! </p>
+st.markdown("""
+<p style="font-size: 22px;">Hey there! </p>
 	
-	<p style="font-size: 22px;">This app here uses the power of machine learning(traditional and neural) to help predict the affliction of an individual with Coronary Heart Disease or Myocardial Infraction(Heart Attack). </p>
-	<p style="font-size: 28px;"><b>
-	By no means is this a medical tool and only a certified doctor can inquire further in your problem.
-	Irrelevant of the result, have periodic visits at the doctor to know more about the body's condition.
-	</b></p>
+<p style="font-size: 22px;">This app here uses the power of machine learning(traditional and neural) to help predict the affliction of an individual with Coronary Heart Disease or Myocardial Infraction(Heart Attack). </p>
+<p style="font-size: 28px;"><b>
+By no means is this a medical tool and only a certified doctor can inquire further in your problem.
+Irrelevant of the result, have periodic visits at the doctor to know more about the body's condition.
+</b></p>
 	
-	<p style="font-size: 22px;">Now, let's see what the AI says about your situation! (Press Proceed at the top) </p>
-	""", unsafe_allow_html=True)
+<p style="font-size: 22px;">Now, let's see what the AI says about your situation! (Press Proceed at the top) </p>
+""", unsafe_allow_html=True)
 	
 	
 
